2b36dbbb894213fdf919f49dcf0e2c02e6d0c7db
Joel Shellman <joel@mentics.com>
2024-04-27 08:10:59 -0700

    Add max_labelled_event_id

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use shared_types::Inferred;
 pub struct KVStore {
     session: Session,
     prep_infer: PreparedStatement,
+    prep_max_label_id: PreparedStatement,
 }
 
 impl KVStore {
@@ -14,7 +15,8 @@ impl KVStore {
         let session = util::create_session().await?;
         Self::setup_db(&session).await?;
         let prep_infer = session.prepare(util::INSERT_INFERRED).await?;
-        Ok(KVStore { session, prep_infer })
+        let prep_max_label_id = session.prepare(util::MAX_LABEL_ID).await?;
+        Ok(KVStore { session, prep_infer, prep_max_label_id })
     }
 
     /// TODO: not happy about exposing QueryResult to callers
@@ -41,11 +43,30 @@ impl KVStore {
         session.query(r#"
             CREATE TABLE IF NOT EXISTS ml_demo.inference (
                 id bigint,
-                time bigint,
+                timestamp bigint,
                 inference float,
                 PRIMARY KEY(id)
             );
         "#, &[]).await?;
+
+        session.query(r#"
+            CREATE TABLE IF NOT EXISTS ml_demo.label (
+                id bigint,
+                version int,
+                timestamp bigint,
+                label varchar,
+                PRIMARY KEY(id, version)
+            );
+        "#, &[]).await?;
+
         Ok(())
+    }
+
+    pub async fn max_labelled_event_id(&self, version: u32) -> anyhow::Result<u64> {
+        Ok(self.session
+                .execute(&self.prep_max_label_id, (version as i32,)).await?
+                .maybe_first_row_typed::<(i64,)>()?
+                .map(|row| row.0 as u64)
+                .unwrap_or(0))
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -8,6 +8,11 @@ pub(crate) static SELECT_INFERRED: &str = r#"
     SELECT time, inference FROM ml_demo.inference WHERE id=?
 "#;
 
+pub(crate) static MAX_LABEL_ID: &str = r#"
+    SELECT MAX(id) FROM ml_demo.label WHERE version=?
+"#;
+
+
 pub(crate) async fn create_session() -> anyhow::Result<Session> {
     let endpoint = std::env::var("SCYLLA_ENDPOINT").unwrap();
     SessionBuilder::new().known_node(endpoint).build().await.map_err(From::from)
