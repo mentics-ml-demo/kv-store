b52b50206ce0f58611cc0d8f1a755a48c7899ffc
Joel Shellman <joel@mentics.com>
2024-06-03 18:53:00 -0700

    label, train, predict basic process working

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,45 @@
+// #![feature(slice_flatten)]
+
 mod util;
 
+use std::iter::zip;
+
 use anyhow::{anyhow, bail};
-use scylla::{cql_to_rust::FromRowError, prepared_statement::PreparedStatement, transport::query_result::SingleRowTypedError, Session};
+use convert::to_model_input_flat;
+use scylla::{cql_to_rust::FromRowError, frame::response::result::Row, prepared_statement::PreparedStatement, transport::query_result::SingleRowTypedError, Session};
 use shared_types::*;
 use util::*;
 
 pub struct KVStore {
-    version: VersionType,
+    version: i32,
     session: Session,
+
     prep_label: PreparedStatement,
-    // prep_labeled_next: PreparedStatement,
-    // prep_labeled_max_id: PreparedStatement,
     prep_label_max: PreparedStatement,
     prep_label_by_id: PreparedStatement,
-    prep_train: PreparedStatement,
-    // prep_max_train_id: PreparedStatement,
-    // prep_infered: PreparedStatement,
-    prep_train_loss: PreparedStatement,
+
+    prep_train_insert: PreparedStatement,
+    prep_train_in_ids: PreparedStatement,
+
+    prep_train_loss_insert: PreparedStatement,
+    prep_train_top_loss: PreparedStatement,
+    prep_train_loss_update: PreparedStatement,
 }
 
 type LabelStoredRow = (i64, i64, i32, i64, i64, Vec<f32>);
-type TrainStoredRow = (i64, i64, i32, i64, f32, Vec<f32>, Vec<f32>);
+type TrainStoredRow = (i64, i64, i32, i64, Vec<f32>, Vec<f32>);
+type TrainLossStoredRow = (i64, i64, f32);
+// type TrainFullStoredRow = (i64, i64, i32, i64, Vec<f32>, Vec<f32>, f32);
+pub struct TrainFull {
+    pub event_id: EventId,
+    pub timestamp: Timestamp,
+    pub partition: PartitionId,
+    pub offset: OffsetId,
+    pub input_flat: Vec<f32>,
+    pub label_flat: Vec<f32>,
+    pub loss: f32,
+}
+
 
 // fn tuple_to_labeled(tup: LabelStoredRow) -> anyhow::Result<LabelStored> {
 //     let value = to_arr(tup.4)?;
@@ -59,21 +78,24 @@ impl KVStore {
     pub async fn new(version: VersionType) -> anyhow::Result<KVStore> {
         let session = create_session().await?;
         Self::setup_db(&session).await?;
-        let prep_labeled = session.prepare(INSERT_LABELED).await?;
-        // let prep_labeled_next = session.prepare(NEXT_LABELED).await?;
-        // let prep_labeled_max_id = session.prepare(MAX_LABELED_ID).await?;
+        let prep_label = session.prepare(INSERT_LABELED).await?;
         let prep_label_max = session.prepare(LABEL_MAX).await?;
         let prep_label_by_id = session.prepare(LABELED_BY_ID).await?;
-        let prep_train = session.prepare(INSERT_TRAINED).await?;
-        let prep_train_loss = session.prepare(SELECT_TRAINED_LOSS).await?;
-        // let prep_infered = session.prepare(INSERT_INFERRED).await?;
+        let prep_train_insert = session.prepare(INSERT_TRAINED).await?;
+        let prep_train_in_ids = session.prepare(TRAIN_IN_IDS).await?;
+        let prep_train_loss_insert = session.prepare(INSERT_TRAIN_LOSS).await?;
+        let prep_train_top_loss = session.prepare(TRAIN_TOP_LOSS).await?;
+        let prep_train_loss_update = session.prepare(UPDATE_TRAIN_LOSS).await?;
 
-        Ok(KVStore { version, session, prep_label: prep_labeled, prep_label_by_id, prep_label_max, prep_train, prep_train_loss })
+        Ok(KVStore { version: version as i32, session,
+            prep_label, prep_label_by_id, prep_label_max,
+            prep_train_insert, prep_train_in_ids,
+            prep_train_loss_insert, prep_train_top_loss, prep_train_loss_update })
     }
 
     pub async fn label_store(&self, labeled: &LabelStored) -> anyhow::Result<()> {
         self.session.execute(&self.prep_label, (
-            self.version as i32, labeled.event_id as i64, labeled.timestamp,
+            self.version, labeled.event_id as i64, labeled.timestamp,
             labeled.partition, labeled.offset_from, labeled.offset_to,
             labeled.label.value.to_vec()
         )).await?;
@@ -83,7 +105,7 @@ impl KVStore {
     pub async fn label_max(&self) -> anyhow::Result<Option<LabelStored>> {
         match self.session.execute(
             &self.prep_label_max,
-            (self.version as i32,)).await?
+            (self.version,)).await?
             .single_row_typed::<LabelStoredRow>() {
             Ok(row) => {
                 Some(tuple_to_label_stored(row)).transpose()
@@ -126,18 +148,98 @@ impl KVStore {
 
     pub async fn train_store(&self, trained: TrainStored) -> anyhow::Result<()> {
         let input = to_model_input_flat(trained.input).to_vec();
-        self.session.execute(&self.prep_train, (
-            self.version as i32, trained.event_id as i64, trained.timestamp,
+        self.session.execute(&self.prep_train_insert, (
+            self.version, trained.event_id as i64, trained.timestamp,
             trained.partition, trained.offset,
-            trained.train.loss, input, trained.label.value.to_vec()
+            input, trained.label.value.to_vec()
         )).await?;
         Ok(())
     }
 
-    pub async fn train_loss(&self, count: u16) -> anyhow::Result<Vec<TrainStoredRow>> {
+    async fn train_in_ids(&self, ids: &[i64]) -> anyhow::Result<Vec<TrainStoredRow>> {
         Ok(self.session
-                .execute(&self.prep_train_loss, (self.version as i32, count as i32)).await?
+                .execute(&self.prep_train_in_ids, (
+                    self.version, ids
+                    // ids.iter().map(|id| *id as i64).collect::<Vec<i64>>()
+                )).await?
                 .rows_typed::<TrainStoredRow>()?.collect::<Result<Vec<TrainStoredRow>,FromRowError>>()?)
+    }
+
+    pub async fn train_loss_store(&self, trained: TrainLossStored) -> anyhow::Result<()> {
+        self.session.execute(&self.prep_train_loss_insert, (
+            self.version, trained.event_id as i64, trained.timestamp, trained.loss
+        )).await?;
+        Ok(())
+    }
+
+    pub async fn train_loss_update(&self, event_id: EventId, timestamp: Timestamp, prev_loss: ModelFloat, new_loss: ModelFloat) -> anyhow::Result<()> {
+        // println!("Updating loss from {} to {} for event {}", prev_loss, new_loss, event_id);
+        self.session.execute(&self.prep_train_loss_update, (
+            self.version, prev_loss, event_id as i64, self.version, event_id as i64, timestamp, new_loss
+        )).await?;
+        Ok(())
+    }
+
+    pub async fn train_top_full(&self, count: usize) -> anyhow::Result<Vec<TrainFull>> {
+        let mut top_loss = self.train_top_loss(count).await?;
+        top_loss.sort_unstable_by_key(|row| row.0);
+
+        self.train_full_from_loss(top_loss).await
+    }
+
+    pub async fn train_oldest_full(&self, count: usize) -> anyhow::Result<Vec<TrainFull>> {
+        let mut top_loss = self.train_top_oldest(count).await?;
+        top_loss.sort_unstable_by_key(|row| row.0);
+
+        self.train_full_from_loss(top_loss).await
+    }
+
+    async fn train_full_from_loss(&self, train_loss: Vec<TrainLossStoredRow>) -> Result<Vec<TrainFull>, anyhow::Error> {
+        let (ids, losses): (Vec<_>, Vec<_>) = train_loss.iter().map(|row| (row.0, row.2)).unzip();
+        let trains = self.train_in_ids(&ids).await?;
+        assert!(train_loss.len() == trains.len());
+        for i in 0..train_loss.len() {
+            assert!(ids[i] == trains[i].0)
+        }
+        let res = zip(trains, losses).map(|(train, loss)|
+            TrainFull {
+                event_id: train.0 as EventId, timestamp: train.1,
+                partition: train.2, offset: train.3,
+                input_flat: train.4, label_flat: train.5,
+                loss
+            }
+            // (train.0, train.1, train.2, train.3, train.4, train.5, loss)
+        ).collect::<Vec<_>>();
+        Ok(res)
+    }
+
+    async fn train_top_loss(&self, count: usize) -> anyhow::Result<Vec<TrainLossStoredRow>> {
+        Ok(self.session
+                .execute(&self.prep_train_top_loss, (self.version, count as i32)).await?
+                .rows_typed::<TrainLossStoredRow>()?.collect::<Result<Vec<TrainLossStoredRow>,FromRowError>>()?)
+    }
+
+    async fn train_top_oldest(&self, count: usize) -> anyhow::Result<Vec<TrainLossStoredRow>> {
+        let rows1 = self.session.query("SELECT MIN(timestamp) FROM ml_demo.train_loss", &[]).await?;
+        let row = rows1.single_row()?;
+        let value = match row.columns[0].as_ref() {
+            None => return Ok(vec![]),
+            Some(value) => value
+        };
+
+        let max_timestamp = value.as_bigint().unwrap();
+
+        let rows_raw = self.session.query(format!("SELECT event_id, timestamp, loss FROM ml_demo.train_loss WHERE timestamp <= {} LIMIT {} ALLOW FILTERING", max_timestamp + 10000, 2*count), &[]).await?;
+        // 1717282813489
+        // SELECT event_id, timestamp, loss FROM ml_demo.train_loss WHERE timestamp <= 1717282823489 LIMIT 64 ALLOW FILTERING;
+        let mut rows = rows_raw.rows_typed::<TrainLossStoredRow>()?.collect::<Result<Vec<TrainLossStoredRow>,FromRowError>>()?;
+        rows.sort_unstable_by_key(|row| row.1);
+        if rows.len() < count {
+            println!("WARNING: Not enough rows {} for top oldest for timestamp: {}", rows.len(), max_timestamp);
+        }
+        rows.truncate(count);
+
+        Ok(rows)
     }
 
     // ----
@@ -171,35 +273,47 @@ impl KVStore {
                 timestamp bigint,
                 partition int,
                 offset bigint,
-                loss float,
                 input frozen<list<float>>,
                 label frozen<list<float>>,
+                PRIMARY KEY(version, event_id)
+            );
+        "#, &[]).await?;
+
+        session.query(r#"
+            CREATE TABLE IF NOT EXISTS ml_demo.train_loss (
+                version int,
+                event_id bigint,
+                timestamp bigint,
+                loss float,
                 PRIMARY KEY(version, loss, event_id)
             ) WITH CLUSTERING ORDER BY (loss DESC, event_id ASC);
         "#, &[]).await?;
 
-        session.query(r#"
-        CREATE TABLE IF NOT EXISTS ml_demo.inferred (
-            id bigint,
-            timestamp bigint,
-            inference frozen<list<float>>,
-            PRIMARY KEY(id)
-        );
-    "#, &[]).await?;
-
-/*
-CREATE INDEX loss_index ON ml_demo.trained (version, loss);
-
- */
+        // session.query(r#"
+        //     CREATE TABLE IF NOT EXISTS ml_demo.inferred (
+        //         id bigint,
+        //         timestamp bigint,
+        //         inference frozen<list<float>>,
+        //         PRIMARY KEY(id)
+        //     );
+        // "#, &[]).await?;
 
         Ok(())
     }
-}
 
-pub fn to_model_input(x: ModelInputFlat) -> ModelInput {
-    bytemuck::cast(x)
-}
+    pub async fn reset_label_data(&self) -> anyhow::Result<()> {
+        self.session.query("TRUNCATE ml_demo.labeled;", &[]).await?;
+        Ok(())
+    }
 
-pub fn to_model_input_flat(x: ModelInput) -> ModelInputFlat {
-    bytemuck::cast(x)
+    pub async fn reset_train_data(&self) -> anyhow::Result<()> {
+        self.session.query("TRUNCATE ml_demo.trained;", &[]).await?;
+        self.session.query("TRUNCATE ml_demo.train_loss;", &[]).await?;
+        Ok(())
+    }
+
+    pub async fn run_query(&self, query: &str) -> anyhow::Result<Vec<Row>> {
+        let result = self.session.query(query, &[]).await?;
+        Ok(result.rows()?)
+    }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,8 @@
 use scylla::{Session, SessionBuilder};
 
 pub(crate) static INSERT_LABELED: &str = r#"
-    INSERT INTO ml_demo.labeled (version, event_id, timestamp, partition, offset_from, offset_to, label) VALUES (?, ?, ?, ?, ?, ?, ?);
+INSERT INTO ml_demo.labeled (version, event_id, timestamp, partition, offset_from, offset_to, label)
+VALUES (?, ?, ?, ?, ?, ?, ?);
 "#;
 
 // pub(crate) static NEXT_LABELED: &str = r#"
@@ -13,11 +14,17 @@ pub(crate) static INSERT_LABELED: &str = r#"
 // "#;
 
 pub(crate) static LABEL_MAX: &str = r#"
-    SELECT event_id, timestamp, partition, offset_from, offset_to, label FROM ml_demo.labeled WHERE version=? ORDER BY event_id DESC LIMIT 1
+SELECT event_id, timestamp, partition, offset_from, offset_to, label
+FROM ml_demo.labeled
+WHERE version=?
+ORDER BY event_id DESC
+LIMIT 1
 "#;
 
 pub(crate) static LABELED_BY_ID: &str = r#"
-    SELECT event_id, timestamp, partition, offset_from, offset_to, label FROM ml_demo.labeled WHERE version=? AND event_id=?
+SELECT event_id, timestamp, partition, offset_from, offset_to, label
+FROM ml_demo.labeled
+WHERE version=? AND event_id=?
 "#;
 
 // pub(crate) static MAX_TRAINED_ID: &str = r#"
@@ -25,12 +32,45 @@ pub(crate) static LABELED_BY_ID: &str = r#"
 // "#;
 
 pub(crate) static INSERT_TRAINED: &str = r#"
-    INSERT INTO ml_demo.trained (version, event_id, timestamp, partition, offset, loss, input, label) VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+INSERT INTO ml_demo.trained (version, event_id, timestamp, partition, offset, input, label)
+VALUES (?, ?, ?, ?, ?, ?, ?);
 "#;
 
-pub(crate) static SELECT_TRAINED_LOSS: &str = r#"
-    SELECT event_id, timestamp, partition, offset, loss, input, label FROM ml_demo.trained WHERE version=? ORDER BY loss DESC LIMIT ?
+pub(crate) static TRAIN_IN_IDS: &str = r#"
+SELECT event_id, timestamp, partition, offset, input, label
+FROM ml_demo.trained
+WHERE version=? AND event_id IN ?
+ORDER BY event_id ASC
 "#;
+// SELECT version, event_id, timestamp, partition, offset, input, label FROM ml_demo.trained WHERE version=1 AND event_id IN (1,2,3)
+
+pub(crate) static INSERT_TRAIN_LOSS: &str = r#"
+INSERT INTO ml_demo.train_loss (version, event_id, timestamp, loss)
+VALUES (?, ?, ?, ?);
+"#;
+
+pub(crate) static TRAIN_TOP_LOSS: &str = r#"
+SELECT event_id, timestamp, loss FROM ml_demo.train_loss
+WHERE version=?
+ORDER BY loss DESC
+LIMIT ?
+"#;
+// , event_id ASC
+// SELECT event_id, timestamp, loss FROM ml_demo.train_loss WHERE version=1 ORDER BY loss DESC LIMIT 3;
+
+pub(crate) static UPDATE_TRAIN_LOSS: &str = r#"
+BEGIN BATCH
+DELETE FROM ml_demo.train_loss WHERE version=? AND loss=? AND event_id=?;
+INSERT INTO ml_demo.train_loss (version, event_id, timestamp, loss) VALUES (?, ?, ?, ?);
+APPLY BATCH;
+"#;
+
+/*
+BEGIN BATCH
+  DELETE FROM ml_demo.train_loss WHERE version=1 AND loss=1.0 AND event_id=1;
+  INSERT INTO ml_demo.train_loss (version, event_id, timestamp, partition, offset, loss, input, label) VALUES (1, 1, 1, 1, 1, 1.7, [1.0, 2.0], [3.0, 4.0]);
+APPLY BATCH;
+ */
 
 // pub(crate) static INSERT_INFERRED: &str = r#"
 //     INSERT INTO ml_demo.inference (id, time, inference) VALUES (?, ?, ?);
